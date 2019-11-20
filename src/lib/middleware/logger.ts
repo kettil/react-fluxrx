66e831ef290ae0bf27d5430bbc17a064a0f5db42
Kettil <development@kettil.de>
2019-11-20 17:33:29 +0100

    Add a groupOutput option

--- src/lib/middleware/logger.ts
+++ src/lib/middleware/logger.ts
@@ -5,10 +5,12 @@ const stylesPre = 'color: #9E9E9E; font-weight: bold';
 const stylesMain = 'color: #F20404; font-weight: bold';
 const stylesPost = 'color: #4CAF50; font-weight: bold';
 
-/**
- *
- */
-export const logger = <State>(): middlewareType<State> => {
+const isConsoleGroupDefiend = () =>
+  typeof console.groupCollapsed === 'function' &&
+  typeof console.groupEnd === 'function' &&
+  typeof console.info === 'function';
+
+export const logger = <State>(groupOutput = true): middlewareType<State> => {
   return {
     init: (state, dispatch, updateDirectly) => {
       console.log('init', { state });
@@ -18,14 +20,14 @@ export const logger = <State>(): middlewareType<State> => {
       const { type, payload, ...rest } = action;
       const id = typeof type === 'symbol' ? type.toString() : type;
 
-      try {
-        console.groupCollapsed(`%c action %c ${id}`, 'color: gray; font-weight: lighter;', 'color: inherit;');
+      if (groupOutput && isConsoleGroupDefiend()) {
+        console.groupCollapsed(`%c Action: %c ${id}`, 'color: gray; font-weight: lighter;', 'color: inherit;');
         console.info('%c prev state', stylesPre, state);
-        console.info('%c action    ', stylesMain, id, payload, rest);
+        console.info('%c action    ', stylesMain, payload, rest);
         console.info('%c next state', stylesPost, reducer(state, action));
         console.groupEnd();
-      } catch (_) {
-        console.log('action', action.type, {
+      } else {
+        console.log('action', id, {
           action,
           prevState: state,
           nextState: reducer(state, action),
@@ -37,13 +39,17 @@ export const logger = <State>(): middlewareType<State> => {
 
     error: (err, dispatch, state) => {
       try {
-        console.groupCollapsed('%c error', stylesMain);
-        console.info('%c state  ', stylesPre, state);
-        console.info('%c message', stylesMain, err.message);
-        console.info('%c stack  ', stylesPost, err.stack);
-        console.groupEnd();
-      } catch (_) {
-        console.error(err);
+        if (groupOutput && err instanceof Error && isConsoleGroupDefiend()) {
+          console.groupCollapsed(`%c Error:  %c ${err.message}`, stylesMain, 'color: inherit;');
+          console.info('%c state  ', stylesPre, state);
+          console.info('%c message', stylesMain, err.message);
+          console.info('%c stack  ', stylesPost, err.stack);
+          console.groupEnd();
+        } else {
+          console.error(err);
+        }
+      } catch (err) {
+        console.log(err);
       }
     },
   };
